8147ad01c5862704e610084b6f7fcc0771f422a9
FuseBuildMonkey <buildmonkey@fusepowered.com>
2015-02-06 16:15:24 -0500

    Release v1.38.0

--- Docs/search/functions_a.js
+++ Docs/search/functions_a.js
@@ -6,6 +6,6 @@ var searchData=
   ['setgamedatacollection',['setGameDataCollection',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#a9bb95b0d854076a36bd498434dd7d51f',1,'com::fusepowered::fuseapi::FuseAPI']]],
   ['setmailasreceived',['setMailAsReceived',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#a3c66dcb04904acaf3b5a70a8c8f99a21',1,'com::fusepowered::fuseapi::FuseAPI']]],
   ['setupgcm',['setupGCM',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#acb93d7fe3db4d4825ddc5e7575bf70c1',1,'com::fusepowered::fuseapi::FuseAPI']]],
-  ['startsession',['startSession',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#a98493239387554f061ad10a302b9570d',1,'com::fusepowered::fuseapi::FuseAPI']]],
+  ['startsession',['startSession',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#ada5f1aa8f1495beb07f4f4e1a1e29f9f',1,'com::fusepowered::fuseapi::FuseAPI']]],
   ['suspendsession',['suspendSession',['../classcom_1_1fusepowered_1_1fuseapi_1_1_fuse_a_p_i.html#acedff43f87b5d686bff1f11c38349908',1,'com::fusepowered::fuseapi::FuseAPI']]]
 ];
